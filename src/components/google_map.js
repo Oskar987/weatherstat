9d197791d9ca6d6530e5848834aea73faa535f5b
pavel <pavelkononenko1@gmail.com>
2017-01-29 20:32:37 +0300

    add api key for goole map

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -6,6 +6,7 @@ export default class GMap extends Component{
     render(){
         return(
             <GoogleMapLoader
+                googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyBIvp4SpWJMD4fehsdUGmD7UXv6MtmVNR8'
                 containerElement={ <div style={{height: '100%', width: '100%'}} />}
                 googleMapElement={ 
                     <GoogleMap defaultZoom={ 12 } defaultCenter={{lat: this.props.lat, lng: this.props.lon}}/>
